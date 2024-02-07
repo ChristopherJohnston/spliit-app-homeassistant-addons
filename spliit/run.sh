adacb61dad8dd0d354e1dbfa055e76c82d87461f
Chris Johnston <christopher_johnston@me.com>
2024-02-07 11:53:23 +0000

    add ssl configuration and run in custom https server

--- spliit/run.sh
+++ spliit/run.sh
@@ -4,7 +4,10 @@ set -e
 CONFIG_PATH=/data/options.json
 export POSTGRES_PRISMA_URL=$(jq --raw-output '.postgres_url // empty' $CONFIG_PATH)
 export POSTGRES_URL_NON_POOLING=$(jq --raw-output '.postgres_url_non_pooling // empty' $CONFIG_PATH)
-
+export NEXT_PUBLIC_BASE_URL=https://localhost
+export CERTFILE=$(jq --raw-output '.certfile // empty' $CONFIG_PATH)
+export KEYFILE=$(jq --raw-output '.keyfile // empty' $CONFIG_PATH)
 
 prisma migrate deploy
-npm run start
+# npm run start
+NODE_ENV=production node ./server.js
